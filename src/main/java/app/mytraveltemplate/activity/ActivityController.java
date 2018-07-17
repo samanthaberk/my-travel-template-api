5a04e2047deeba88264191c5af770a94d45016e7
Samantha Berk <sberk10@gmail.com>
2018-07-16 19:35:13 -0700

    can return all tags for a specific city

--- src/main/java/app/mytraveltemplate/activity/ActivityController.java
+++ src/main/java/app/mytraveltemplate/activity/ActivityController.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import app.mytraveltemplate.tag.Tag;
 
 @RestController
 public class ActivityController {
@@ -24,79 +24,9 @@ public class ActivityController {
 		return activityService.filterActivitiesByCity(city);
 	}
 	
-	@PostMapping("/filter/{query}")
-	public List<Activity> filterActivities(@RequestBody ActivityQuery query) {
-		return activityService.filterActivities(query);
+	@RequestMapping("/filter/{city}/{timeOfDay}")
+	public List<Tag> filterActivities(@PathVariable String city, @PathVariable String timeOfDay) {
+		return activityService.filterActivitiesInCity(city, timeOfDay);
 	}
-	
-	
-	public class ActivityQuery {	
-		private String city;
-		private String timeOfDay;
-		private String type;
-		private String budget;
-		private String pace;
-		private String sites;
-		private List<String> interests;
-		private List<String> entertainment;
-		private List<String> transport;
-		
-		public String getCity() {
-			return city;
-		}
-		public void setCity(String city) {
-			this.city = city;
-		}
-		
-		public String getTimeOfDay() {
-			return timeOfDay;
-		}
-		public void setTimeOfDay(String timeOfDay) {
-			this.timeOfDay = timeOfDay;
-		}
-		public String getType() {
-			return type;
-		}
-		public void setType(String type) {
-			this.type = type;
-		}
-		public String getBudget() {
-			return budget;
-		}
-		public void setBudget(String budget) {
-			this.budget = budget;
-		}
-		public String getPace() {
-			return pace;
-		}
-		public void setPace(String pace) {
-			this.pace = pace;
-		}
-		public String getSites() {
-			return sites;
-		}
-		public void setSites(String sites) {
-			this.sites = sites;
-		}
-		public List<String> getInterests() {
-			return interests;
-		}
-		public void setInterests(List<String> interests) {
-			this.interests = interests;
-		}
-		public List<String> getEntertainment() {
-			return entertainment;
-		}
-		public void setEntertainment(List<String> entertainment) {
-			this.entertainment = entertainment;
-		}
-		public List<String> getTransport() {
-			return transport;
-		}
-		public void setTransport(List<String> transport) {
-			this.transport = transport;
-		}
-		
-	}
-	
+
 }
--- src/main/java/app/mytraveltemplate/activity/ActivityService.java
+++ src/main/java/app/mytraveltemplate/activity/ActivityService.java
@@ -6,18 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import app.mytraveltemplate.activity.ActivityController.ActivityQuery;
 import app.mytraveltemplate.tag.Tag;
-import app.mytraveltemplate.tag.TagRepository;
 
 @Service
 public class ActivityService {
 	
 	@Autowired
 	private ActivityRepository activityRepository;
-	
-	@Autowired 
-	private TagRepository tagRepository;
 	
 	public List<Activity> getAllActivities() {
 		List<Activity> activities = new ArrayList<>();
@@ -38,37 +33,49 @@ public class ActivityService {
 		return activities;
 	}
 
-	public List<Activity> filterActivities(ActivityQuery query) {
-		List<Activity> activities = new ArrayList<>();
+//	public List<Activity> filterActivities(ActivityQuery query) {
+//		List<Activity> activities = new ArrayList<>();
+//		
+//		for(Activity a : activityRepository.findAll()) {
+//			// find activities for specified city
+//			if(
+//				a.getCity().equals(query.getCity()) 
+//			  )
+//				{
+//					//examine tags for activities in that city
+//					for(Tag t : tagRepository.findAll()) 
+//					{
+//					//does the tag we're looking at match a parameter in the query?
+//						if( 
+//							t.tagname.equals(query.getTimeOfDay()) 
+//							|| t.tagname.equals(query.getPace())
+//							|| t.tagname.equals(query.getType())
+//							|| t.tagname.equals(query.getBudget())
+//							|| t.tagname.equals(query.getSites())
+//							|| query.getInterests().contains(t.tagname)
+//							|| query.getEntertainment().contains(t.tagname)
+//							|| query.getTransport().contains(t.tagname)
+//							)
+//							
+//							//if it does, add the activity to the list
+//								{
+//									activities.add(a);
+//									
+//								}
+//					}
+//				}
+//			}
+//		return activities;
+//	}
+	
+	// filter for timeOfDay, pace, type, budget, and sites
+	public List<Tag> filterActivitiesInCity(String city, String timeOfDay) {
+		List<Activity> activities = filterActivitiesByCity(city); // isolate activities for that city
+		List<Tag> tags = new ArrayList<>(); //initialize a list of each activity's tag
 		
-		for(Activity a : activityRepository.findAll()) {
-			// find activities for specified city
-			if(
-				a.getCity().equals(query.getCity()) 
-			  )
-				{
-					//examine tags for activities in that city
-					for(Tag t : tagRepository.findAll()) 
-					{
-					//does the tag we're looking at match a parameter in the query?
-						if( 
-							t.tagname.equals(query.getTimeOfDay()) || t.tagname.equals(query.getPace())
-							|| t.tagname.equals(query.getType())
-							|| t.tagname.equals(query.getBudget())
-							|| t.tagname.equals(query.getSites())
-							|| query.getInterests().contains(t.tagname)
-							|| query.getEntertainment().contains(t.tagname)
-							|| query.getTransport().contains(t.tagname)
-							)
-							
-							//if it does, add the activity to the list
-								{
-									activities.add(a);
-									
-								}
-					}
-				}
-			}
-		return activities;
+		for (Activity a : activities ) {
+			tags.addAll(a.getTags());
+		}
+		return tags;
 	}
 }
