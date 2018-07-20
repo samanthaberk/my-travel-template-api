e93c2b611adc4066b372b9a9b2ec83e3efd2f450
Samantha Berk <sberk10@gmail.com>
2018-07-20 14:36:30 -0700

    create database for heroku

--- src/main/java/app/mytraveltemplate/SpringMvcConfig.java
+++ src/main/java/app/mytraveltemplate/SpringMvcConfig.java
@@ -3,8 +3,6 @@ package app.mytraveltemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
