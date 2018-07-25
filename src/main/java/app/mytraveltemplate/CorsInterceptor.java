fe0457fb55415f16fbbb295f0fac94899c017f0c
Samantha Berk <sberk10@gmail.com>
2018-07-25 13:41:32 -0700

    cors

--- src/main/java/app/mytraveltemplate/CorsInterceptor.java
+++ src/main/java/app/mytraveltemplate/CorsInterceptor.java
@@ -1,21 +1,21 @@
-//package app.mytraveltemplate;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-//
-//@Component
-//public class CorsInterceptor extends HandlerInterceptorAdapter{
-//	@Override
-//	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-//			ModelAndView modelAndView) throws Exception {
-//		
-//		response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
-//		super.postHandle(request, response, handler, modelAndView);
-//	}
-//		//@Override
-//		//public void postHandle(HtttpServletRequest request, HttpServletResponse response, ModelAndView model)
-//}
+package app.mytraveltemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class CorsInterceptor extends HandlerInterceptorAdapter{
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+		//@Override
+		//public void postHandle(HtttpServletRequest request, HttpServletResponse response, ModelAndView model)
+}
--- src/main/java/app/mytraveltemplate/SpringMvcConfig.java
+++ src/main/java/app/mytraveltemplate/SpringMvcConfig.java
@@ -1,24 +1,24 @@
-//package app.mytraveltemplate;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebMvc
-//public class SpringMvcConfig extends WebMvcConfigurerAdapter {
-//	
-//	@Autowired
-//	CorsInterceptor travelInjectedInterceptor;
-//	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(travelInjectedInterceptor);
-//	}
-//	
-//
-//   
-//}
+package app.mytraveltemplate;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Autowired
+	CorsInterceptor travelInjectedInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(travelInjectedInterceptor);
+	}
+	
+
+   
+}
